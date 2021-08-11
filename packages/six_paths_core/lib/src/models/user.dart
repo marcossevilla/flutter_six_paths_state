ffc46debf5568c9c3d4426715a8d9041a8bc0990
Marcos Sevilla <marcossevilla14@gmail.com>
2021-08-11 17:27:30 -0600

    add analysis_options file to six_paths_core

--- packages/six_paths_core/lib/src/models/user.dart
+++ packages/six_paths_core/lib/src/models/user.dart
@@ -2,14 +2,14 @@ import 'package:uuid/uuid.dart';
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
-  final String id;
-  final String name;
-  final String email;
-
   User({
     required this.name,
     required this.email,
-  }) : this.id = Uuid().v4();
+  }) : id = const Uuid().v4();
+
+  final String id;
+  final String name;
+  final String email;
 
   @override
   List<Object> get props => [id, name, email];
